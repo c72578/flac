f507f19c89a36307273a84a66e07c2a25db9f440
Ralph Giles <giles@thaumas.net>
2021-07-04 07:54:39 -0700

    Improve output when running lldb
    Mark failure and start the debugger on a new line so the log
    is easier to understand.

--- test/test_streams.sh
+++ test/test_streams.sh
@@ -62,6 +62,7 @@ test_file ()
 	echo "### ENCODE $name #######################################################" >> ./streams.log
 	echo "###    cmd=$cmd" >> ./streams.log
 	if ! $cmd 2>>./streams.log; then
+	  echo "FAIL"
 	  lldb --batch --one-line "run $cmd_opts" --one-line-on-crash "bt" flac
 	  die "ERROR during encode of $name"
 	fi
